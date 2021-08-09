2c4bf2b9f65176a25485fd53f51d24b35a838063
rahatN <naz.rahat.hina@gmail.com>
2021-08-08 21:33:37 -0700

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+  return (
+   <section className='section about'>
+     <h1 className='section-title'>About us</h1>
+     <p>For my book The Authority Gap, which looks at why women
+        are still taken less seriously than men, I commissioned 
+         Book Research to find out exactly who was reading what.
+          I wanted to know whether female authors were not just
+           deemed less authoritative than men, but whether they
+            were being read by men in the first place. And the
+             results confirmed my suspicion that
+        men were disproportionately unlikely even to open a book
+         by a woman.
+
+     </p>
+   </section>
+  )
+}
+
+export default About
